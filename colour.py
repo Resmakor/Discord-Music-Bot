daea9f53a3bf51fe6c3b03d68d94f53f284dfb3c
Bartłomiej <52079500+Resmakor@users.noreply.github.com>
2022-09-14 21:18:05 +0200

    Update colour.py
    Small changes

--- colour.py
+++ colour.py
@@ -1,15 +1,16 @@
 from colorthief import ColorThief
 from urllib.request import urlopen
-import io
-import discord
+from io import BytesIO
+from discord import Color
 
 def get_colour(id):
+    """Function finds the most suitable embed colour from YouTube thumbnail"""
     url = f'https://img.youtube.com/vi/{id}/default.jpg'
     fd = urlopen(url)
-    f = io.BytesIO(fd.read())
+    f = BytesIO(fd.read())
     color_thief = ColorThief(f)
     rgb = list(color_thief.get_palette(color_count=6))
     print(rgb)
     which_palette = int(len(rgb) / 2)
-    colour =  discord.Color.from_rgb(rgb[which_palette][0], rgb[which_palette][1], rgb[which_palette][2])
+    colour = Color.from_rgb(rgb[which_palette][0], rgb[which_palette][1], rgb[which_palette][2])
     return colour
