13a141953a1da0817fb96012c899413b6d81ffbb
Bartłomiej <52079500+Resmakor@users.noreply.github.com>
2022-09-14 21:18:35 +0200

    Update search_link.py
    Refactorization

--- search_link.py
+++ search_link.py
@@ -1,41 +1,16 @@
-import urllib.request
-import re
+from colorthief import ColorThief
+from urllib.request import urlopen
+from io import BytesIO
+from discord import Color
 
-
-def polish_signs(url):
-    while 'ą' in url or 'Ą' in url:
-        url = url.replace('ą', 'a')
-        url = url.replace('Ą', 'A')
-    while 'ę' in url or 'Ę' in url:
-        url = url.replace('ę', 'e')
-        url = url.replace('Ę', 'E')
-    while 'ć' in url or 'Ć' in url:
-        url = url.replace('ć', 'c')
-        url = url.replace('Ć', 'C')
-    while 'ż' in url or 'Ż' in url:
-        url = url.replace('ż', 'z')
-        url = url.replace('Ż', 'Z')
-    while 'ż' in url or 'Ź' in url:
-        url = url.replace('ź', 'z')
-        url = url.replace('Ź', 'Z')
-    while 'ł' in url or 'Ł' in url:
-        url = url.replace('ł', 'l')
-        url = url.replace('Ł', 'L')
-    while 'ó' in url or 'Ó' in url:
-        url = url.replace('ó', 'o')
-        url = url.replace('Ó', 'O')
-    while 'ś' in url or 'Ś' in url:
-        url = url.replace('ś', 's')
-        url = url.replace('Ś', 'S')
-    while 'ń' in url or 'Ń' in url:
-        url = url.replace('ń', 'n')
-        url = url.replace('Ń', 'N')
-    return url
-
-def link(url):
-    url = polish_signs(url)
-    url = url.replace(" ", "+")
-    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + str(url))
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-
-    return str("https://www.youtube.com/watch?v=" + str(video_ids[0]))
+def get_colour(id):
+    """Function finds the most suitable embed colour from YouTube thumbnail"""
+    url = f'https://img.youtube.com/vi/{id}/default.jpg'
+    fd = urlopen(url)
+    f = BytesIO(fd.read())
+    color_thief = ColorThief(f)
+    rgb = list(color_thief.get_palette(color_count=6))
+    print(rgb)
+    which_palette = int(len(rgb) / 2)
+    colour = Color.from_rgb(rgb[which_palette][0], rgb[which_palette][1], rgb[which_palette][2])
+    return colour
