ef2c1fc64cd7e1bf745934cf94dcd63a36db40b5
Bartłomiej <52079500+Resmakor@users.noreply.github.com>
2022-09-19 20:30:18 +0200

    Update search_link.py
    Wrong file before

--- search_link.py
+++ search_link.py
@@ -1,16 +1,12 @@
-from colorthief import ColorThief
 from urllib.request import urlopen
-from io import BytesIO
-from discord import Color
+from unidecode import unidecode
+from re import findall
 
-def get_colour(id):
-    """Function finds the most suitable embed colour from YouTube thumbnail"""
-    url = f'https://img.youtube.com/vi/{id}/default.jpg'
-    fd = urlopen(url)
-    f = BytesIO(fd.read())
-    color_thief = ColorThief(f)
-    rgb = list(color_thief.get_palette(color_count=6))
-    print(rgb)
-    which_palette = int(len(rgb) / 2)
-    colour = Color.from_rgb(rgb[which_palette][0], rgb[which_palette][1], rgb[which_palette][2])
-    return colour
+
+def link(user_words):
+    """Function returns YouTube url for first video id found in HTML code"""
+    user_words = unidecode(user_words).replace(" ", "+")
+    html = urlopen("https://www.youtube.com/results?search_query=" + str(user_words))
+    video_ids = findall(r"watch\?v=(\S{11})", html.read().decode())
+    return str("https://www.youtube.com/watch?v=" + str(video_ids[0]))
+
